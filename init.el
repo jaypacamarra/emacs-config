e9ea535f66607baf1e4f90e688b19d7558fe46ae
jay.pacamarra <jay.pacamarra@sanctuary.ai>
2024-01-23 12:03:43 -0800

    update: ivy,counsel,move-buffer

--- init.el
+++ init.el
@@ -1,8 +1,3 @@
-(setq make-backup-files nil)
-(org-babel-load-file
-  (expand-file-name
-    "config.org"
-    user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,3 +11,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq make-backup-files nil)
+(org-babel-load-file
+  (expand-file-name
+    "config.org"
+    user-emacs-directory))
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-sudo add-apt-repository ppa:kelleyk/emacs
-sudo apt-get update
-sudo apt-get remove emacs-common
-sudo apt install emacs28
+#sudo add-apt-repository ppa:kelleyk/emacs
+#sudo apt-get update
+#sudo apt-get remove emacs-common
+#sudo apt install emacs28
 cp config.org init.el early-init.el ~/.config/emacs
 echo "finished setting up emacs"
