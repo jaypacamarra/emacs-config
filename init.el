1b75ba4e8e09b42c7beb84c433c294f4328e552b
jay.pacamarra <jay.pacamarra@sanctuary.ai>
2024-01-24 10:10:32 -0800

    fixes

--- init.el
+++ init.el
@@ -12,7 +12,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279"
+   '("534c78790bf064c6dff04dd001ebc1d3c30385f4c5138d99ad300ae5dc179e04"
+     "bc7d4cfb6d4bd7074a39f97f0b8a057c5b651c403950bbbc4ad35a609ad6268a"
+     "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279"
      "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"
      default)))
 (custom-set-faces
