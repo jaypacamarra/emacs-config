ca307e0d7c4b253a854764d959893b822ea32960
jay.pacamarra <jay.pacamarra@sanctuary.ai>
2024-01-23 19:23:30 -0800

    add: LSP and treesitter

--- init.el
+++ init.el
@@ -1,19 +1,23 @@
 (set-default 'truncate-lines t)
+;;no backup files
+(setq make-backup-files nil)
+;;convert config.org into config.el
+(org-babel-load-file
+  (expand-file-name
+    "config.org"
+    user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tango-dark))
- '(warning-suppress-types '((emacs))))
+ '(custom-safe-themes
+   '("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279"
+     "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"
+     default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(setq make-backup-files nil)
-(org-babel-load-file
-  (expand-file-name
-    "config.org"
-    user-emacs-directory))
